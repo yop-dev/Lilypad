6f2fd7b298bdd1c7bdec411eebdea01d1ea0b09e
Yopp <desilvajoner99@gmail.com>
2024-07-04 14:59:46 +0800

    opera

--- karma.conf.js
+++ karma.conf.js
@@ -3,11 +3,12 @@
 
 module.exports = function (config) {
   config.set({
+    browsers: ['Opera'],
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-opera-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
