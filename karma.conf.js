b1e757f69fcc10436b462c153b7ad4246ac622e0
Yopp <desilvajoner99@gmail.com>
2024-07-08 17:04:21 +0800

    wtf

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', 'karma-typescript'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-opera-launcher'), // Add the Opera GX launcher plugin
+      require('karma-opera-launcher'), // Add the Opera launcher plugin
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('karma-typescript') // Add karma-typescript plugin for handling TypeScript files
@@ -33,12 +33,12 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_DEBUG, // Set log level to DEBUG
     autoWatch: true,
-    browsers: isCI ? ['OperaGX'] : ['Opera'], // Use OperaGX in CI and Opera locally
+    browsers: isCI ? ['Opera'] : ['Opera'], // Use Opera in both CI and local for simplicity
     customLaunchers: {
       OperaGX: {
         base: 'Opera',
         flags: ['--disable-extensions', '--no-sandbox'],
-        binary: 'C:/Users/desil/AppData/Local/Programs/Opera GX/launcher.exe' // Update this path as necessary
+        binary: process.env.OPERA_BIN || 'C:/Users/desil/AppData/Local/Programs/Opera GX/launcher.exe' // Use environment variable if available
       }
     },
     singleRun: isCI,
